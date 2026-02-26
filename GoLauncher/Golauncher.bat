aabf0ae8b24b9bb8d49fa0a08a14bf92227fd005
Enric-xX <almenarenric4@gmail.com>
2026-02-26 22:19:06 +0100

    Delete GoLauncher directory

--- GoLauncher/Golauncher.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:bucle
-:: 'start' necesita saber QUÉ abrir. Usamos 'cmd.exe' para abrir la consola.
-start cmd.exe
-:: 'goto' debe ir al nombre de la etiqueta que pusiste arriba (bucle)
-goto bucle
